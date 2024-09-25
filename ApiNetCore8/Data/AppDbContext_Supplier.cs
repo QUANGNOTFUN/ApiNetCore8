e4a25bfe0dbf006ef0659f64ab73009cd3ea9b38
Pham-Quoc-huy <huypq5102@ut.edu.vn>
2024-09-25 13:44:39 +0700

    da tao supplier

---
+++ ApiNetCore8/Data/AppDbContext_Supplier.cs
@@ -0,0 +1,23 @@
+﻿using ApiNetCore8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiNetCore8.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Thiết lập quan hệ khóa ngoại
+            modelBuilder.Entity<Supplier>()
+                .HasOne(s => s.ProductCategory)
+                .WithMany(pc => pc.Suppliers)
+                .HasForeignKey(s => s.IdPC);
+        }
+    }
+
+}
---
+++ ApiNetCore8/Models/Supplier.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ApiNetCore8.Models;
+namespace ApiNetCore8.Models
+{
+    public class Supplier
+    {
+        public int Id { get; set; } // Khóa chính, tự động tăng
+        public int IdPC { get; set; } // Khóa ngoại đến ProductCategories
+
+        [Required]
+        public string ContactInfo { get; set; } // Thông tin liên lạc
+
+       //danh mục sản phẩm
+        public ProductCategory ProductCategory { get; set; }
+    }
+    public class ProductCategory: Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Supplier> Suppliers { get; set; }
+
+    }
+
+}
