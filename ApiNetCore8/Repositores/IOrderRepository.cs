0255ecb3f16de3bc452c7e56aaaa4acae28f51cc
Pham-Quoc-huy <huypq5102@ut.edu.vn>
2024-10-18 02:13:32 +0700

    sua order vs orderdetail

--- ApiNetCore8/Repositores/IOrderRepository.cs
+++ ApiNetCore8/Repositores/IOrderRepository.cs
@@ -4,12 +4,11 @@ namespace ApiNetCore8.Repositores
 {
     public interface IOrderRepository
     {
-        public Task<List<OrderModel>> GetAllOrderAsync();
-        public Task<OrderModel> GetOrderByIdAsync(int id);
+        public Task<PagedResult<OrderModel>> GetAllOrderAsync(int page, int pageSize);
         public Task<int> AddOrderAsync(OrderModel model);
         public Task UpdateOrderAsync(int id, OrderModel model);
         public Task DeleteOrderAsync(int id);
-        public Task<List<OrderModel>> GetLimitedOrdersAsync(int limit);
-        public Task<List<OrderModel>> SearchOrdersAsync(string searchTerm, int page, int pageSize);
+        public Task<OrderModel> GetOrderByIdAsync(int id);
+        public Task<PagedResult<OrderModel>> FindOrderAsync(string name, int page, int pageSize);
     }
 }
